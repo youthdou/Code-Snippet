98ccbede8d0d3678710dcfb3029dfab296f2b0a1
sww <sww@zqykj.com>
2019-03-21 14:14:06 +0800

    1、目录遍历； 2、excel写功能。

--- snippet.py
+++ snippet.py
@@ -1,6 +1,8 @@
 import datetime
 import time
 import threading
+import os
+import xlwt
 
 #datetime -> string
 def get_str_datetime():
@@ -21,6 +23,36 @@ def thread_demo():
     t.start()
     t.join()
 
+def traverse_dir(dir_name):
+    l_dirs = os.walk(dir_name)
+    l_files = []
+    max_d = 0
+    for root, dirs, files in l_dirs:
+        # print("Root: ", root)
+        max_d = max(len(root.split("\\")), max_d)
+        # for d in dirs:
+        #     # print("D: ", d)
+        #     pass
+        # for f in files:
+        #     # print("F: ", f)
+        #     l_files.append((root, f))
+        l_files.append((root, files))
+    # print("max depth: ", max_d)
+    # print(l_files)
+    return l_files, max_d
+
+def write_excel(l_files, max_d):
+    workbook = xlwt.Workbook(encoding = 'utf-8')
+    worksheet = workbook.add_sheet('src')
+    for i in range(len(l_files)):
+        root, files = l_files[i]
+        worksheet.write(i, 0, root)
+        worksheet.write(i, 1, files)
+    workbook.save('vat-src.xls')
+
+
 if __name__ == '__main__':
-    thread_demo()
+#     thread_demo()
+    l_files, max_d = traverse_dir('src')
+    write_excel(l_files, max_d)
     pass
